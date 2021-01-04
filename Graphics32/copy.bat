7ab52729805a4839b21906409edb67231bb3f5e1
civilian7 <civilian7@gmail.com>
2021-01-05 01:02:28 +0900

    Add files via upload

---
+++ Graphics32/copy.bat
@@ -0,0 +1,6 @@
+copy .\Source\*.inc ..\..\LIB\DCU\Win32\Debug
+copy .\Source\*.inc ..\..\LIB\DCU\Win32\Release
+copy .\Source\*.inc ..\..\LIB\DCU\Win64\Debug
+copy .\Source\*.inc ..\..\LIB\DCU\Win64\Release
+
+pause
